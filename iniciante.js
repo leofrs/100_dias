084546ff65a3be80d99efcdd29b7250b5465c802
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-10-16 20:00:23 -0300

    Dia 3

--- iniciante.js
+++ iniciante.js
@@ -1,3 +1,11 @@
 // 1
 const fiveNumbersList = [1, 2, 3, 4, 5];
 // fiveNumbersList.forEach((number) => console.log(`Número: ${number} `));
+
+//2
+fiveNumbersList.push(6);
+console.log(fiveNumbersList);
+
+//3
+fiveNumbersList.splice(2, 1);
+console.log(fiveNumbersList);
