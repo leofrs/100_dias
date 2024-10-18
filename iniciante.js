b7ba2b397f4ce9db748a56fe3413291da11143b6
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-10-18 09:08:49 -0300

    Dia 5 feito

--- iniciante.js
+++ iniciante.js
@@ -1,6 +1,6 @@
 // 1
 const fiveNumbersList = [1, 2, 3, 4, 5];
-// fiveNumbersList.forEach((number) => console.log(`Número: ${number} `));
+// console.log(fiveNumbersList);
 
 //2
 fiveNumbersList.push(6);
@@ -13,3 +13,10 @@ console.log(fiveNumbersList);
 //4
 fiveNumbersList.reverse();
 console.log(fiveNumbersList);
+
+//5
+const stringList = ["Leo", "Emmily"];
+for (let i = 0; i < stringList.length; i++) {
+  const element = stringList[i];
+  console.log(`O elemento na posição ${i}, tem ${element.length} letras`);
+}
